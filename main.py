6d4c15a64f014b0b1fe0dce0b951d61b385ac1f7
Moe <Moealaouie@gmail.com>
2024-05-25 18:55:20 -0400

    adding us states game

--- main.py
+++ main.py
@@ -12,18 +12,30 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+
+    answer_state = screen.textinput(title = "Guess the State", prompt = "What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 
-answer_state = screen.textinput(title = "Guess the State", prompt = "What's another state's name?")
-
-
-if answer_state in states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state)
 
 
 screen.exitonclick()
